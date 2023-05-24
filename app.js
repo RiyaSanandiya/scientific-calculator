96213ba0618167e565d1210197932107a646ee81
RiyaSanandiya <129592522+RiyaSanandiya@users.noreply.github.com>
2023-05-24 11:19:41 +0530

    add more function

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ function calculate(){
 
 
 // function to calculate the sin, cosine and tan of a angle in DEG(degree)
-function sin(){
+function sine(){
   screen.value = Math.sin(screen.value) * (Math.PI / 180);
 }
 
@@ -86,14 +86,53 @@ function tan(){
 }
 
 // function to calculate the asin, acos and atan of a angle in DEG(degree)
-function asin(){
+function invsine(){
   screen.value = Math.sin(screen.value) * (180 / Math.PI);
 }
 
-function acos(){
+function invcosine(){
   screen.value = Math.sin(screen.value) * (180 / Math.PI);
 }
 
-function atan(){
+function invtan(){
   screen.value = Math.sin(screen.value) * (180 / Math.PI);
+}
+
+// function to calculate the natural logarithm 
+function ln(){
+  screen.value = Math.log(screen.value)
+}
+
+function log(){
+  screen.value = Math.log10(screen.value)
+}
+
+// function for percentage
+function percentage(){
+  screen.value = (screen.value / 100)
+}
+
+// function for square, cube and square root
+function square(){
+  screen.value = Math.pow(screen.value, 2)
+}
+function cube(){
+  screen.value = Math.cube(screen.value, 3)
+}
+function sqrt(){
+  screen.value = Math.sqrt(screen.value)
+}
+
+
+// function for +/-(plusMinus)
+function plusminus(){
+  var x = screen;
+  var y = x.value;
+  y - y * -1;
+  x.value - y;
+}
+
+// Exp
+function Exp(){
+  screen.value = Math.pow(10, screen.value)
 }
